71e378d1f507c84071bba152b977544ab55cbc32
Pruthil Ghori <112156454+pruthilghori@users.noreply.github.com>
2025-02-27 18:34:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Chandan Tiwari!
+          Hello Pruthil Ghori!
         </a>
       </header>
     </div>
